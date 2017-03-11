768efbe44785c3d684db5e0bdc6ce8e62b18f513
Alan Gutierrez <alan@prettyrobots.com>
2017-03-10 21:07:34 -0600

    Bring over Nascent Coalesce.

---
+++ extant.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+    for (var i = 0, I = arguments.length; i < I; i++) {
+        if (arguments[i] != null) {
+            return arguments[i]
+        }
+    }
+    return null
+}
--- t/extant.t.js
+++ t/extant.t.js
@@ -1,6 +1,7 @@
-require('proof/redux')(1, prove)
+require('proof/redux')(2, prove)
 
 function prove (assert) {
     var coalesce = require('..')
-    assert(!! coalesce, 'require')
+    assert(coalesce(null, null), null, 'null')
+    assert(coalesce(null, (function () {})(), 1), 1, 'coalesced')
 }
