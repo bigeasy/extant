b96395ff5e5a615189fddc4c1a8c1c71addddf48
Alan Gutierrez <alan@prettyrobots.com>
2019-09-29 02:16:20 -0500

    Revert tests to Proof.
    See #18.

--- test/extant.test.js
+++ test/extant.test.js
@@ -1,8 +1,5 @@
-describe('extant', () => {
-    const assert = require('assert')
-    it('can coalesce', () => {
-        const coalesce = require('..')
-        assert(coalesce(null, null) == null, 'null')
-        assert.equal(coalesce(null, (function () {})(), 1), 1, 'coalesced')
-    })
+require('proof')(2, (okay) => {
+    const coalesce = require('..')
+    okay(coalesce(null, null), null, 'null')
+    okay(coalesce(null, (function () {})(), 1), 1, 'coalesced')
 })
