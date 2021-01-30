4a22763c45e78eaa97d55c758e954d96b85e2994
Alan Gutierrez <alan@prettyrobots.com>
2021-01-30 09:06:45 -0600

    Export `coalesce`.

--- extant.js
+++ extant.js
@@ -1,4 +1,4 @@
-module.exports = function () {
+exports.coalesce = function () {
     for (var i = 0, I = arguments.length; i < I; i++) {
         if (arguments[i] != null) {
             return arguments[i]
--- test/extant.t.js
+++ test/extant.t.js
@@ -1,5 +1,5 @@
 require('proof')(2, (okay) => {
-    const coalesce = require('..')
+    const { coalesce } = require('..')
     okay(coalesce(null, null), null, 'null')
     okay(coalesce(null, (function () {})(), 1), 1, 'coalesced')
 })
