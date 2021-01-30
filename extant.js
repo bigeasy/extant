af2bc68275ee35a351a71c82a8716e683806abf0
Alan Gutierrez <alan@prettyrobots.com>
2021-01-30 09:10:19 -0600

    Write some Docco.

--- extant.js
+++ extant.js
@@ -1,3 +1,8 @@
+// # Extant
+
+// Return the first non-null-like argument. Much like SQL COALESCE.
+
+//
 exports.coalesce = function () {
     for (var i = 0, I = arguments.length; i < I; i++) {
         if (arguments[i] != null) {
